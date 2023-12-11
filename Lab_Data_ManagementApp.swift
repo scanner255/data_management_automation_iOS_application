5bbde2e57e04e632ca36a85f3fdbe6d779d47a17
Riley Thompson <95446494+scanner255@users.noreply.github.com>
2023-12-11 13:19:40 -0600

    Add files via upload

---
+++ Lab_Data_ManagementApp.swift
@@ -0,0 +1,19 @@
+//
+//  Lab_Data_ManagementApp.swift
+//  Lab Data Management
+//
+//  Created by William Thompson on 11/18/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Lab_Data_ManagementApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
